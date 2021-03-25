b96733aead469784242704fc5bf8356cb7039573
MichaelDumas <60619461+MichaelDumas@users.noreply.github.com>
2021-03-25 12:35:38 -0400

    Added Expenses and Categories windows to the MainWindow
    Made the buttons on the MainWindow functional and implemented adding categories and expenses using the GUI

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,11 @@ namespace WPF
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            MessageBox.Show("Made it to app exit ", "yay", MessageBoxButton.OK, MessageBoxImage.Information);
+            MainWindow window = new MainWindow();
+            window.budget.CloseDB();
+        }
     }
 }
